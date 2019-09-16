dd44a3837b2b4dbc2e674b7b254f4760a2682448
Ilya Katun <40162026+hypnospinner@users.noreply.github.com>
2019-09-16 14:45:23 +0300

    Implement reading .psd file header

--- ArtArea/ArtArea.Parsing/Psd/Builder/PSDBuilder.cs
+++ ArtArea/ArtArea.Parsing/Psd/Builder/PSDBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace ArtArea.Parse.Psd.Builder
@@ -46,8 +47,91 @@ namespace ArtArea.Parse.Psd.Builder
             }
         }
 
+        /// <summary>
+        /// Reades Header part of .psd/.psb file
+        /// </summary>
+        /// <param name="psd">Psd file that should be filled with new header values</param>
+        /// <param name="stream">Stream to read .psd file</param>
         private static void ReadHeader(PsdFile psd, FileStream stream)
         {
+            // read file signature & chack if this file is valid .psd file
+            string signature = stream.ReadUsAsciiString(4);
+
+            if (!string.Equals(signature, Signature, StringComparison.Ordinal)) 
+                throw new PsdBuildException(
+                    $"File signature is not valid. " +
+                    $"File signature \"{signature}\", expected signature \"{Signature}\"");
+
+            // read file version (defines whether it .psd or .psb) & check if it's valid
+            short version = stream.ReadBigEndianInt16();
+
+            if (version < 1 || version > 2)
+                throw new PsdBuildException(
+                    $"File version is not valid. " +
+                    $"File version \"{version}\", expected version 1 or 2");
+
+            psd.Version = version;
+
+            // read 6 reserved bytes & check if all of them are 0
+            byte[] reserved = stream.ReadBytes(6);
+
+            if (reserved.Any(b => b != 0x00))
+                throw new PsdBuildException($"Non-zero bytes in reserved 6 bytes section");
+
+            // read number of channels & check if it is limited correctly
+            short numberOfChannels = stream.ReadBigEndianInt16();
+
+            if (numberOfChannels < PsdFile.MinChannels || numberOfChannels > PsdFile.MaxChannels)
+                throw new PsdBuildException($"Number of channels is out of bounds. " +
+                    $"Number if channels {numberOfChannels} should be in [ {PsdFile.MinChannels}, {PsdFile.MaxChannels} ]");
+
+            psd.NumberOfChannels = numberOfChannels;
+
+            // read file height
+            int height = stream.ReadBigEndianInt32();
+
+            if (height < 1)
+                throw new PsdBuildException($"File height is invalid. File height {height} should be > 0");
+
+            // dependending on version check if file height is not more tham limit
+            if (version == 1 ?
+                height > PsdFile.PsdMaxDimention :
+                height > PsdFile.PsbMaxDemension)
+                throw new PsdBuildException(
+                    $"File height is invalid. " +
+                    $"File height shouldn't be more than {PsdFile.PsdMaxDimention} for .psd and {PsdFile.PsbMaxDemension} for .psb");
+
+            psd.Height = height;
+
+            // read file width
+            int width = stream.ReadBigEndianInt32();
+
+            if (width < 1)
+                throw new PsdBuildException($"File width is invalid. File width {width} should be > 0");
+
+            // dependending on version check if file width is not more tham limit
+            if (version == 1 ?
+                width > PsdFile.PsdMaxDimention :
+                width > PsdFile.PsbMaxDemension)
+                throw new PsdBuildException(
+                    $"File width is invalid. " +
+                    $"File width shouldn't be more than {PsdFile.PsdMaxDimention} for .psd and {PsdFile.PsbMaxDemension} for .psb");
+
+            psd.Width = width;
+
+            // read depth & chekc if it takes valid values
+            short depth = stream.ReadBigEndianInt16();
+            if (depth != 1 && depth != 8 && depth != 16 && depth != 32)
+                throw new PsdBuildException($"File depth is invalid. File depth is {depth} and should be 1, 8, 16, 32");
+
+            psd.Depth = depth;
+
+            // read color mode, check if it's valid & convert to enum
+            short colorMode = stream.ReadBigEndianInt16();
+            if (!Enum.IsDefined(typeof(ColorMode), colorMode))
+                throw new PsdBuildException($"Color mode takes invalid value. Color mode {colorMode}");
+
+            psd.ColorMode = (ColorMode) colorMode;
         }
 
         private static void ReadColorModeData(PsdFile psd, FileStream stream)
@@ -64,6 +148,7 @@ namespace ArtArea.Parse.Psd.Builder
 
         private static void CreateImagedataPlaceholder(PsdFile psd, FileStream stream)
         {
+
         }   
     }
 }
