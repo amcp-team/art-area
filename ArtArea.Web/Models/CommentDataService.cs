0a38ccefcbb13674910024fa998890fa3f06dc7a
SabinaAlieva <sabina-alieva.taganrog@mail.ru>
2019-10-26 17:01:02 +0300

    Realised Comment data service

--- ArtArea.Web/Models/CommentDataService.cs
+++ ArtArea.Web/Models/CommentDataService.cs
@@ -3,30 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using System.IO;
+using System.Collections.Generic;
+using MongoDB.Driver;
 
 namespace ArtArea.Web.Models
 {
     public class CommentDataService : ICommentDataService
     {
         public ApplicationDb db;
-        public Task<Issue> AddComment(Comment comment)
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task<Comment> GetComment(string id)
-        {
-            throw new NotImplementedException();
-        }
+        public CommentDataService(ApplicationDb database)
+            => db = database;
 
-        public Task<IEnumerable<Comment>> GetComments()
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task<IEnumerable<Comment>> GetCommentsByFile(ObjectId FileId)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<Comment> GetComment(string id)
+            => await db.Comments.Find(x => x.Id == new ObjectId(id)).FirstOrDefaultAsync();
+        public async Task<IEnumerable<Comment>> GetComments()
+            => await db.Comments.Find(x => true).ToListAsync();
+
+        public async Task<IEnumerable<Comment>> GetCommentsByFile(ObjectId FileId)
+            => await db.Comments.Find(x => x.FileId == FileId).ToListAsync();
+
+        public async Task AddComment(Comment comment)
+            => await db.Comments.InsertOneAsync(comment);
+        
     }
 }
