0d1fae08087821c6690815edc794f672f5c77d63
Ilya Katun <40162026+hypnospinner@users.noreply.github.com>
2019-10-26 16:21:12 +0300

    add file controller & filedata view model

---
+++ ArtArea.Web/ViewModels/FileData.cs
@@ -0,0 +1,9 @@
+namespace ArtArea.Web.ViewModels
+{
+    public class FileData
+    {
+        public string FileType{get;set;}
+        public string Base64{get;set;}
+        public string Name{get;set;}
+    }
+}
