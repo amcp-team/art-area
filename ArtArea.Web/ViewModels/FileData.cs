b297030e92484864cc9c69370ed750e978ee397a
Ilya Katun <40162026+hypnospinner@users.noreply.github.com>
2019-10-26 17:02:25 +0300

    improve FileData ViewModel

--- ArtArea.Web/ViewModels/FileData.cs
+++ ArtArea.Web/ViewModels/FileData.cs
@@ -2,6 +2,7 @@ namespace ArtArea.Web.ViewModels
 {
     public class FileData
     {
+        public string Id {get;set;}
         public string FileType{get;set;}
         public string Base64{get;set;}
         public string Name{get;set;}
