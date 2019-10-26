a75fc312fb97fe40c80c9801ea4cb34c5b1b171e
AndyS1mpson <werter1555rus@mail.ru>
2019-10-26 21:37:42 +0300

    add User and User Data Service

--- ArtArea.Web/Models/ApplicationDb.cs
+++ ArtArea.Web/Models/ApplicationDb.cs
@@ -20,7 +20,7 @@ public class ApplicationDb
     public IMongoCollection<File> Files => database.GetCollection<File>("files");
     public IMongoCollection<Comment> Comments => database.GetCollection<Comment>("comments");
     public IMongoCollection<Project> Projects => database.GetCollection<Project>("projects");
-
+    public IMongoCollection<User> Users => database.GetCollection<User>("user");
 
 
 }
---
+++ ArtArea.Web/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtArea.Web.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string email { get; set; }
+        //public string Avatar { get; set; }
+        public string Description { get; set; }
+        //public string IdMSSQL { get; set; }
+    }
+}
