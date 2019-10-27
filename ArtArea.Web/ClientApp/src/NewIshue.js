1308fc3cadeb35da8c3c77981eabec8d7b0b9b1f
flamingo221100 <flamingo221100@gmail.com>
2019-10-27 03:53:18 +0300

    start model of create canvas page

--- ArtArea.Web/ClientApp/src/NewIshue.js
+++ ArtArea.Web/ClientApp/src/NewIshue.js
@@ -6,9 +6,26 @@ export class NewIshue extends React.Component{
         desription:"",
     }
 
+    
+
     render(){
         console.log(this.state)
-        return
+        return(
+            <>
+            <div className="row">
+                <div className="col-12">
+                    <div className="form-group">
+                        <input onInput={this.onNameChange} type="email" className="form-control form-group" placeholder="Name of Ishue"/>
+
+                        <textarea onInput={this.onTextCommentChange} className="form-control form-group"  aria-label="With textarea" placeholder="Enter description"></textarea>
+                        <button type="button" onClick={this.addComment}  className="btn btn-primary form-group">Create ishue</button>
+                    </div> 
+                </div>
+
+            </div>
+            
+            </>
+        )
     }
 }
 
