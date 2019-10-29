6578c53c35814c06b18831ae8b1f5aba22b82656
hypnospinner <hypnospinner@gmail.com>
2019-10-29 23:21:19 +0300

    setup simplest functional web api

--- ArtArea/ArtArea.Api/Program.fs
+++ ArtArea/ArtArea.Api/Program.fs
@@ -1,8 +1,25 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open Giraffe
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Hosting
 
-open System
+let routes =
+    choose [
+        route "/" >=> text "Hello World from F#!"
+    ]
+
+let configureApp(app:IApplicationBuilder) =
+    app.UseGiraffe routes
+
+let configureServices(services:IServiceCollection) =
+    services.AddGiraffe |> ignore
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main _ =
+    WebHostBuilder()
+        .UseKestrel()
+        .Configure(configureApp)
+        .ConfigureServices(configureServices)
+        .Build()
+        .Run()
+    0
