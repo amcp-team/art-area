df6c3ecb07e0572f878f3a8ac9d241badef5dc41
hypnospinner <hypnospinner@gmail.com>
2019-11-15 21:47:26 +0300

    add todos & irepos

--- ArtArea/ArtArea.Web.Data/Interface/IUserRepository.cs
+++ ArtArea/ArtArea.Web.Data/Interface/IUserRepository.cs
@@ -1,6 +1,6 @@
 ﻿namespace ArtArea.Web.Data.Interface
 {
-    // TODO - dev CRUD interface for user collection in db
+    // TODO - CRUD interface for user entity in db
     //      - get user by id, name
     //      - post new user
     //      - change user by id, name (passing new user object)
