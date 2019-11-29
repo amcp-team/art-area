783c17b41893191817cbc60cb322d2a3ef174a9b
Victoria Dzyuba <47743417+mohorka@users.noreply.github.com>
2019-11-29 17:18:43 +0300

    Add Project Controller
    Add Controller and GetProject method

---
+++ src/ArtArea.Web/Controllers/ProjectController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ArtArea.Web.Data.Interface;
+
+namespace ArtArea.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+       
+        private IProjectRepository _projectRepository;
+
+        public ProjectController(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        [Produces("application/json")]
+        [HttpGet("project/{projectId}")]
+        public async Task<IActionResult> GetProject(string id) 
+        {
+            var projects = await _projectRepository.ReadProjects();
+            return new ObjectResult(projects.FirstOrDefault(p=>p.Id==id));
+        
+        }
+    }
+}
