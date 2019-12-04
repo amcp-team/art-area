72754f6b5e44d4cf973794ce7ad32eaeecbe903d
hypnospinner <hypnospinner@gmail.com>
2019-12-04 21:35:01 +0300

    setup Caliburn.Micro w/ basic DI

---
+++ src/ArtArea.Desktop/Bootstrapper.cs
@@ -0,0 +1,51 @@
+﻿using ArtArea.Desktop.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace ArtArea.Desktop
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+        private bool _databaseConfigured = false;
+        public Bootstrapper() => Initialize();
+
+        protected override void Configure()
+        {
+            _container = _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            typeof(App).Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.Contains("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType =>
+                    _container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        #region DI Simple Container Utils
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+            => _container.GetAllInstances(service);
+
+        protected override object GetInstance(Type service, string key)
+            => _container.GetInstance(service, key);
+
+        protected override void BuildUp(object instance)
+            => _container.BuildUp(instance);
+
+        #endregion
+    }
+}
---
+++ src/ArtArea.Desktop/ViewModels/ShellViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArtArea.Desktop.ViewModels
+{
+    class ShellViewModel
+    {
+    }
+}
