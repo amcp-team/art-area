a07230ad27d0b7c3f60d16bdd7b97554e40f53cf
Victoria Dzyuba <47743417+mohorka@users.noreply.github.com>
2019-12-05 22:29:03 +0300

    add board controller and board service
    todo: add GetPinData and Pin Service

---
+++ src/ArtArea.Web/Services/BoardService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArtArea.Models;
+using ArtArea.Web.Data.Interface;
+
+namespace ArtArea.Web.Services
+{
+    public class BoardService
+    {
+        private IBoardRepository _boardRepository;
+        public BoardService(IBoardRepository boardRepository)
+        {
+            _boardRepository = boardRepository;
+        }
+
+        public bool BoardExists(string boardId)
+            => _boardRepository.ReadBoard(boardId) != null;
+
+        public Task<Board> GetBoardAsync(string boardId)
+        {
+            if (BoardExists(boardId))
+            {
+                return _boardRepository.ReadBoardAsync(boardId);
+            }
+            else throw new Exception("No board with this id");
+
+        }
+        
+
+      
+    }
+}
---
+++ src/ArtArea.Web/Services/PinService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArtArea.Models;
+using ArtArea.Web.Data.Interface;
+
+namespace ArtArea.Web.Services
+{
+    public class PinService
+    {
+        
+    }
+}
